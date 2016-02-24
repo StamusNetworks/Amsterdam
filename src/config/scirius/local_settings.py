64de03091669e3127e0f068ec1bf9ee2a72a47db
Eric Leblond <eric@regit.org>
2016-02-24 14:02:46 +0100

    scirius: add kibana + evebox reverse proxy

--- src/config/scirius/local_settings.py
+++ src/config/scirius/local_settings.py
@@ -5,11 +5,15 @@ ELASTICSEARCH_ADDRESS = "elasticsearch:9200"
 ELASTICSEARCH_2X = True
 KIBANA_VERSION=4
 KIBANA_INDEX = ".kibana"
-KIBANA_URL = "http://localhost:5601"
+KIBANA_URL = "http://kibana:5601"
 
 SURICATA_UNIX_SOCKET = "/var/run/suricata/suricata-command.socket"
 
 USE_KIBANA = True
+KIBANA_PROXY = True
+
+USE_EVEBOX = True
+EVEBOX_ADDRESS = "evebox:5636"
 
 USE_SURICATA_STATS = True
 USE_LOGSTASH_STATS = True
