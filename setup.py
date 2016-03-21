00a1c289ba7cacaa302a97e8a1abedee8dc5cc95
Eric Leblond <eric@regit.org>
2016-03-21 09:26:49 +0100

    amsterdam: fix installer
    Fix a error at installation. THis should also allow to install
    dependencies automatically during `pip install`.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='amsterdam',
       package_dir={'amsterdam':'src'},
       package_data={'amsterdam': data_files},
       provides=['amsterdam'],
-      requires=['argparse', 'docker-compose'],
+      install_requires=['argparse', "docker-compose",],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
