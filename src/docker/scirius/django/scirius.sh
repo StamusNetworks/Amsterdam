9edd731cd62fe62a5d2803c90bc6e7e0a9c9c629
Peter Manev <petermanev@gmail.com>
2016-04-11 15:42:30 +0200

    scirius: Update ETOpen ruleset download link location.

--- src/docker/scirius/django/scirius.sh
+++ src/docker/scirius/django/scirius.sh
@@ -12,7 +12,7 @@ create_db() {
 	python manage.py migrate
 	python manage.py loaddata /opt/selks/scirius/scirius.json
 	python manage.py createcachetable my_cache_table
-	python manage.py addsource "ETOpen Ruleset" https://rules.emergingthreats.net/open/suricata-2.0.7/emerging.rules.tar.gz http sigs
+	python manage.py addsource "ETOpen Ruleset" https://rules.emergingthreats.net/open/suricata-3.0/emerging.rules.tar.gz http sigs
 	python manage.py addsource "SSLBL abuse.ch" https://sslbl.abuse.ch/blacklist/sslblacklist.rules http sig
 	python manage.py defaultruleset "Default SELKS ruleset"
 	python manage.py disablecategory "Default SELKS ruleset" stream-events
